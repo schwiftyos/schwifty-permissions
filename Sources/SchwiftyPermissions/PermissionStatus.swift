68d9d9efe3a2fa252e79a407e8bc5b26906e6096
RandomHashTags <imrandomhashtags@gmail.com>
2025-02-21 19:07:18 -0600

    fixes #1

--- Sources/SchwiftyPermissions/PermissionStatus.swift
+++ Sources/SchwiftyPermissions/PermissionStatus.swift
@@ -5,6 +5,13 @@
 //  Created by Evan Anderson on 2/6/25.
 //
 
+// TODO: fix (Module [Foundation, FoundationEssentials] was not compiled with library evolution support; using it means binary compatibility for 'SchwiftyPermissions' can't be guaranteed)
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#elseif canImport(Foundation)
+import Foundation
+#endif
+
 /// List of available statuses a permission can have.
 public enum PermissionStatus : Hashable, Sendable {
     /// Access is never allowed.
@@ -25,8 +32,10 @@ public enum PermissionStatus : Hashable, Sendable {
     /// Access is only allowed if granted by the user, when requested.
     case uponRequest
 
+    #if canImport(FoundationEssentials) || canImport(Foundation)
     /// Access is only allowed temporarily, until a set time.
-    case temporarilyUntil(ContinuousClock.Instant)
+    case temporarilyUntil(Date)
+    #endif
 
     /// Access is only allowed temporarily, between two given times of day.
     //case temporarilyBetween(Double, Double) // TODO: support
@@ -42,7 +51,11 @@ extension PermissionStatus {
         case .onlyInBackground: return state == .background
         case .onlyInForeground: return state == .foreground
         case .uponRequest: return true // TODO: fix
-        case .temporarilyUntil(let expires): return ContinuousClock.now <= expires
+
+        #if canImport(FoundationEssentials) || canImport(Foundation)
+        case .temporarilyUntil(let expires): return Date.now <= expires
+        #endif
+
         @unknown default: return false
         }
     }
