93c73b186c35d2cfa50292be5168d3ba09e349f4
RandomHashTags <imrandomhashtags@gmail.com>
2025-02-23 03:31:12 -0600

    use spaces instead of tabs in `Program.swift`

--- Sources/SchwiftyPermissions/Program.swift
+++ Sources/SchwiftyPermissions/Program.swift
@@ -6,62 +6,62 @@
 //
 
 public struct Program : Sendable {
-	public typealias ApplicationID = String
-	public typealias ProcessID = UInt64
-	
-	public let applicationID:ApplicationID
-	public let pid:ProcessID
+    public typealias ApplicationID = String
+    public typealias ProcessID = UInt64
 
-	/// Process owner.
-	/// 
-	/// - Warning: All programs are minimally owned by the root process
-	public private(set) var ownerpid:ProcessID
+    public let applicationID:ApplicationID
+    public let pid:ProcessID
 
-	/// Current processing state of this program.
-	public private(set) var state:ProgramState
+    /// Process owner.
+    /// 
+    /// - Warning: All programs are minimally owned by the root process
+    public private(set) var ownerpid:ProcessID
 
-	init(
-		ownerpid: ProcessID = 0,
-		pid: ProcessID,
-		state: ProgramState = .notRunning,
-		applicationID: String
-	) {
-		self.applicationID = applicationID
-		self.pid = pid
-		self.ownerpid = ownerpid
-		self.state = state
-	}
+    /// Current processing state of this program.
+    public private(set) var state:ProgramState
+
+    init(
+        ownerpid: ProcessID = 0,
+        pid: ProcessID,
+        state: ProgramState = .notRunning,
+        applicationID: String
+    ) {
+        self.applicationID = applicationID
+        self.pid = pid
+        self.ownerpid = ownerpid
+        self.state = state
+    }
 }
 
 // MARK: Equatable
 extension Program : Equatable {
-	@inlinable
-	public static func == (lhs: Program, rhs: Program) -> Bool {
-		return lhs.ownerpid == rhs.ownerpid && lhs.pid == rhs.pid
-	}
+    @inlinable
+    public static func == (lhs: Program, rhs: Program) -> Bool {
+        return lhs.ownerpid == rhs.ownerpid && lhs.pid == rhs.pid
+    }
 }
 
 // MARK: Hashable
 extension Program : Hashable {
-	@inlinable
-	public func hash(into hasher: inout Hasher) {
-		hasher.combine(ownerpid)
-		hasher.combine(pid)
-	}
+    @inlinable
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ownerpid)
+        hasher.combine(pid)
+    }
 }
 
 // MARK: Permissions
 extension Program {
-	/// Request the use of a permission.
-	/// 
-	/// - Parameters:
-	///   - permission: Permission you want to request.
-	///   - reason: Reason why the program is requesting the permission.
-	@inlinable
-	public func request<T: SchwiftyPermission>(
-		_ permission: SchwiftyPermissionType,
-		reason: String
-	) async -> Result<T, PermissionError> {
-		return await PermissionStorage.shared.permissions(for: self).request(permission, for: self, reason: reason)
-	}
+    /// Request the use of a permission.
+    /// 
+    /// - Parameters:
+    ///   - permission: Permission you want to request.
+    ///   - reason: Reason why the program is requesting the permission.
+    @inlinable
+    public func request<T: SchwiftyPermission>(
+        _ permission: SchwiftyPermissionType,
+        reason: String
+    ) async -> Result<T, PermissionError> {
+        return await PermissionStorage.shared.permissions(for: self).request(permission, for: self, reason: reason)
+    }
 }
