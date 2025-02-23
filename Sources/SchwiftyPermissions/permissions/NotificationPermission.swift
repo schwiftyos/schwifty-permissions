a111dabc35f0393917127276c5add99d20c1ef9d
Evan Anderson <ImRandomHashTags@gmail.com>
2025-02-22 20:17:05 -0600

    Update NotificationPermission.swift
    sync with main; previous commit was wrong file

--- Sources/SchwiftyPermissions/permissions/NotificationPermission.swift
+++ Sources/SchwiftyPermissions/permissions/NotificationPermission.swift
@@ -1,76 +1,69 @@
 //
-//  NetworkPermission.swift
+//  NotificationPermission.swift
 //
 //
 //  Created by Evan Anderson on 2/17/25.
 //
 
-/// Network permissions for a program.
-public struct NetworkPermission : SchwiftyPermission {
+/// Notification permissions for a program.
+public struct NotificationPermission : SchwiftyPermission {
     public private(set) var status:PermissionStatus
 
-    /// URLs the program is allowed to contact.
-    public private(set) var urlWhitelist:Set<String>
-
-    /// URLs the program is not allowed to contact.
-    public private(set) var urlBlacklist:Set<String>
+    @usableFromInline
+    var permissions:UInt8
 
     @usableFromInline
-    var downloadPermissions:ConnectionType.RawValue
-
-    @usableFromInline
-    var uploadPermissions:ConnectionType.RawValue
+    var sendPermissions:AlertType.RawValue
 }
 
 // MARK: Default
-extension NetworkPermission {
+extension NotificationPermission {
     public static let `default`:Self = Self(
         status: .uponRequest,
-        urlWhitelist: [],
-        urlBlacklist: [],
-        downloadPermissions: .max,
-        uploadPermissions: .max
+        permissions: .max,
+        sendPermissions: .max
     )
 }
 
-// MARK: ConnectionType
-extension NetworkPermission {
-    public enum ConnectionType : UInt8, Sendable {
-        case local    = 1
-        case wired    = 2
-        case wireless = 4
-        case hotspot  = 8
-        case cellular = 16
-        case vpn      = 32
+// MARK: AlertType
+extension NotificationPermission {
+    public enum AlertType : UInt8, Sendable {
+        /// Regular notification.
+        case normal        = 1
+
+        /// Warning, hazard, critical notification.
+        case critical      = 2
+
+        // TODO: add documentation
+        case timeSensitive = 4
+    }
+
+    @inlinable
+    public func canSend(_ alertType: AlertType = .normal) -> Bool {
+        return canSend && (sendPermissions & alertType.rawValue != 0)
     }
 }
 
-// MARK: Download
-extension NetworkPermission {
-    /// Whether or not a program can download data over the specified connection.
+// MARK: Send
+extension NotificationPermission {
     @inlinable
-    public func canDownload(over connection: ConnectionType) -> Bool {
-        return downloadPermissions & connection.rawValue != 0
-    }
-
-    /// Whether or not a program can download data from the specified URL over the specified connection.
-    @inlinable
-    public func canDownload(from url: String, over connection: ConnectionType) -> Bool {
-        return canDownload(over: connection) && (urlWhitelist.isEmpty || urlWhitelist.contains(url)) && !urlBlacklist.contains(url)
+    public var canSend : Bool {
+        permissions & 0b1 != 0
     }
 }
 
-// MARK: Update
-extension NetworkPermission {
-    /// Whether or not a program can upload data over the specified connection.
+// MARK: Badge
+extension NotificationPermission {
     @inlinable
-    public func canUpload(over connection: ConnectionType) -> Bool {
-        return uploadPermissions & connection.rawValue != 0
+    public var canShowBadge : Bool {
+        permissions & 0b01 != 0
     }
+}
 
-    /// Whether or not a program can upload data to the specified URL over the specified connection.
+// MARK: Sound
+extension NotificationPermission {
     @inlinable
-    public func canUpload(to url: String, over connection: ConnectionType) -> Bool {
-        return canUpload(over: connection) && (urlWhitelist.isEmpty || urlWhitelist.contains(url)) && !urlBlacklist.contains(url)
+    public var canPlaySound : Bool {
+        permissions & 0b001 != 0
     }
 }
