b5d057b6edf89d7df9bd9734e0b8564cb5c0f448
Evan Anderson <ImRandomHashTags@gmail.com>
2025-12-11 02:04:35 +0000

    update notification permissions (#29)

--- Sources/SchwiftyPermissions/permissions/NotificationPermission.swift
+++ Sources/SchwiftyPermissions/permissions/NotificationPermission.swift
@@ -5,9 +5,23 @@ public struct NotificationPermission: SchwiftyPermission {
 
     public private(set) var status:PermissionStatus
 
-    var permissions:UInt8
+    var permissions:Flag.RawValue
 
     var sendPermissions:AlertType.RawValue
+}
+
+// MARK: Flag
+extension NotificationPermission {
+    enum Flag: UInt8, Sendable {
+        case send      = 1
+        case showBadge = 2
+        case playSound = 4
+
+        /// - Returns: If the flag is enabled in the given `permissions`.
+        func isEnabled(_ permissions: RawValue) -> Bool {
+            permissions & rawValue > 0
+        }
+    }
 }
 
 // MARK: Default
@@ -30,30 +44,66 @@ extension NotificationPermission {
 
         // TODO: add documentation
         case timeSensitive = 4
+
+        func isEnabled(_ permissions: RawValue) -> Bool {
+            permissions & rawValue > 0
+        }
     }
 
-    public func canSend(_ alertType: AlertType = .normal) -> Bool {
-        return canSend && (sendPermissions & alertType.rawValue != 0)
+    /// - Returns: If a notification with the given `AlertType` can be sent.
+    public func canSendAlertType(_ alertType: AlertType) -> Bool {
+        return canSend && alertType.isEnabled(sendPermissions)
     }
 }
 
 // MARK: Send
 extension NotificationPermission {
+    /// - Returns: If sending a notification is allowed.
     public var canSend: Bool {
-        permissions & 0b1 != 0
+        Flag.send.isEnabled(permissions)
     }
 }
 
 // MARK: Badge
 extension NotificationPermission {
+    /// - Returns: If showing a notification badge is allowed.
     public var canShowBadge: Bool {
-        permissions & 0b01 != 0
+        Flag.showBadge.isEnabled(permissions)
     }
 }
 
 // MARK: Sound
 extension NotificationPermission {
+    /// - Returns: If playing a notification sound is allowed.
     public var canPlaySound: Bool {
-        permissions & 0b001 != 0
+        Flag.playSound.isEnabled(permissions)
+    }
+}
+
+// MARK: Modifications
+extension NotificationPermission {
+    /// Mutates `permissions` to reflect the new status for the given `flag`.
+    private mutating func updateFlag(_ flag: Flag, isEnabled: Bool) {
+        permissions = isEnabled ? permissions | flag.rawValue : permissions & ~flag.rawValue
+    }
+
+    /// Mutates `permissions` to reflect the new `send` flag status.
+    mutating func setCanSend(_ newValue: Bool) {
+        updateFlag(.send, isEnabled: newValue)
+    }
+
+    /// Mutates `permissions` to reflect the new `showBadge` flag status.
+    mutating func setCanShowBadge(_ newValue: Bool) {
+        updateFlag(.showBadge, isEnabled: newValue)
+    }
+
+    /// Mutates `permissions` to reflect the new `playSound` flag status.
+    mutating func setCanPlaySound(_ newValue: Bool) {
+        updateFlag(.playSound, isEnabled: newValue)
+    }
+
+    /// Mutates `sendPermissions` to reflect the new alert type flag status.
+    mutating func setCanSendAlertType(_ alertType: AlertType, newValue: Bool) {
+        sendPermissions = newValue ? sendPermissions | alertType.rawValue : sendPermissions & ~alertType.rawValue
     }
 }
